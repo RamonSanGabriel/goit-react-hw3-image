dd4e532b250557a90013292ad8f8f09519b5e67c
RamonSanGabriel <jeshero11@gmail.com>
2025-03-14 17:54:03 +0800

    modal

--- goit-react-hw3-image/src/components/Modal/MenuModal/MenuModal.jsx
+++ goit-react-hw3-image/src/components/Modal/MenuModal/MenuModal.jsx
@@ -4,16 +4,18 @@ import './MenuModal.css';
 const MenuModal = ({ onClose }) => {
   return (
     <div className="overlay">
+      {/* <div className="close-button container"> */}
+      <button className="close-button" onClick={onClose}>
+        &times;
+        {/* <p>close</p> */}
+      </button>
+      {/* </div> */}
       <div className="menu-modal">
-        <button className="close-modal" onClick={onClose}>
-          &times;
-          <p>close</p>
-        </button>
         <div className="nav-links">
           <ul className="nav-list">
             {navbar.map(({ id, name, link }) => (
-              <div className="nav-list-items">
-                <li key={id}>{name}</li>
+              <div className="container nav-list-items" key={id}>
+                <li>{name}</li>
               </div>
             ))}
           </ul>

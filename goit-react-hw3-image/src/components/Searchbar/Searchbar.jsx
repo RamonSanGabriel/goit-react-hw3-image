3d488eb444c5663461cf8ea68d9f6ec24c2b4af0
RamonSanGabriel <jeshero11@gmail.com>
2025-03-14 22:12:04 +0800

    update MenuModal.css

--- goit-react-hw3-image/src/components/Searchbar/Searchbar.jsx
+++ goit-react-hw3-image/src/components/Searchbar/Searchbar.jsx
@@ -2,11 +2,22 @@ import './Searchbar.css';
 import { FiSearch } from 'react-icons/fi';
 
 const Searchbar = () => {
+  const handleSearch = (e) => {
+    // alert('Search button clicked');
+    e.preventDefault();
+    const searchInput = document.querySelector('.search-input');
+    console.log(searchInput);
+    const searchValue = searchInput.value.trim();
+
+    if (searchValue) {
+      window.location.href = `/search/${searchValue}`;
+    }
+  };
   return (
     <div className="search-wrapper">
       <form action="" className="search-form">
         <button className="search-btn">
-          <FiSearch />
+          <FiSearch onClick={handleSearch} />
         </button>
         {/* <label htmlFor="search" className="search-label"> */}
         <input

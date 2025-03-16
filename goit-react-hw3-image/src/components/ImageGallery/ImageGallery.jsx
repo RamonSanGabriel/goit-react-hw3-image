23d88bd9109b79861fd1fe1e64db424cf1259a4b
RamonSanGabriel <jeshero11@gmail.com>
2025-03-16 21:07:54 +0800

    gallery

--- goit-react-hw3-image/src/components/ImageGallery/ImageGallery.jsx
+++ goit-react-hw3-image/src/components/ImageGallery/ImageGallery.jsx
@@ -6,11 +6,17 @@ const ImageGallery = () => {
     <div className="image-gallery">
       <h1>Gallery</h1>
       <ul className="gallery-list">
-        {imageGallery.map(({ img, id }) => (
+        {imageGallery.map(({ img, id, src }) => (
           <div key={id} className="image-gallery container">
             <li className="gallery-items">
-              {/* <img src="https://picsum.photos/200/300" alt="Image 1" /> */}
+              {/* <a href={src}> */}
+              <img
+                className="gallery-image"
+                src="https://picsum.photos/200/300"
+                alt="Image 1"
+              />
               {img}
+              {/* </a> */}
             </li>
           </div>
         ))}
--- goit-react-hw3-image/src/data/image-gallery.js
+++ goit-react-hw3-image/src/data/image-gallery.js
@@ -2,17 +2,21 @@ export const imageGallery = [
   {
     id: 1,
     img: 'image 1',
+    src: 'https://picsum.photos/200/300" alt="Image 1',
   },
   {
     id: 2,
     img: 'image 2',
+    src: 'https://picsum.photos/200/300" alt="Image 2',
   },
   {
     id: 3,
     img: 'image 3',
+    src: 'https://picsum.photos/200/300" alt="Image 3',
   },
   {
     id: 4,
     img: 'image 4',
+    src: 'https://picsum.photos/200/300" alt="Image 4',
   },
 ];
